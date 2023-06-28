ddd05afcf5435530d7fdbf26caa39a91b32fb373
MagenX <magenx@users.noreply.github.com>
2023-06-28 13:31:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,22 +58,17 @@ resource "random_password" "this" {
 # Create load balancer
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "digitalocean_loadbalancer" "this" {
-  name                   = digitalocean_projetc.this.name
-  region                 = var.region
-  vpc_uuid               = var.vpc_uuid
-  size                   = var.size
-  algorithm              = var.algorithm
-
-  enable_proxy_protocol  = true
-  redirect_http_to_https = true
+  name        = digitalocean_projetc.this.name
+  region      = var.region
+  vpc_uuid    = digitalocean_vpc.this.id
+  project_id  = digitalocean_project.this.id
+  size        = var.size
   
   forwarding_rule {
     entry_port     = 80
     entry_protocol = "http"
-
     target_port     = 80
     target_protocol = "http"
-
   }
 
   healthcheck {
@@ -85,7 +80,123 @@ resource "digitalocean_loadbalancer" "this" {
     healthy_threshold        = 5
   }
   
-  droplet_tag = "loadbalancer"
+  droplet_tag = "frontend"
+}
+//////////////////////////////////////////////////[ DIGITALOCEAN RESERVED IP ]////////////////////////////////////////////
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create DigitalOcean reserved ip address
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "digitalocean_reserved_ip" "this" {
+  region = var.region
+}
+
+resource "digitalocean_reserved_ip_assignment" "this" {
+  ip_address = digitalocean_reserved_ip.this.ip_address
+  droplet_id = digitalocean_droplet.this["varnish"].id
+}
+////////////////////////////////////////////////////[ DIGITALOCEAN DROPLETS ]/////////////////////////////////////////////
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create DigitalOcean droplets
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "digitalocean_droplet" "this" {
+  for_each      = var.droplets
+  image         = "debian-11"
+  name          = "${digitalocean_project.this.name}-${each.key}"
+  region        = var.region
+  size          = var.size
+  monitoring    = true
+  vpc_uuid      = digitalocean_vpc.this.id
+  ssh_keys      = [digitalocean_ssh_key.this.fingerprint]
+  volume_ids    = each.key == "media" ? [digitalocean_volume.this.id] : null
+  resize_disk   = false
+  droplet_agent = true
+  tags          = [
+    each.key,
+    digitalocean_tag.this.id
+  ]
+}
+/////////////////////////////////////////////////[ DIGITALOCEAN MEDIA VOLUME ]////////////////////////////////////////////
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create volume for media droplet storage
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "digitalocean_volume" "this" {
+  region                  = var.region
+  name                    = "${digitalocean_project.this.name}-media-volume"
+  size                    = 120
+  initial_filesystem_type = "ext4"
+  description             = "Volume for media droplet @ ${digitalocean_project.this.name}"
+}
+//////////////////////////////////////////[ DIGITALOCEAN DROPLETS CONFIGURATION ]/////////////////////////////////////////
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create DigitalOcean droplets configuration
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "terraform_data" "this" {
+  for_each = var.droplets
+
+  connection {
+      host        = digitalocean_droplet.this[each.key].ipv4_address
+      type        = "ssh"
+      user        = "root"
+      private_key = tls_private_key.this.public_key_openssh
+    }
+
+  provisioner "remote-exec" {
+    inline = [
+      "cloud-init --file /tmp/cloud-init-config-${each.key}.yaml"
+    ]
+  }
+
+  provisioner "file" {
+    content     = <<-EOF
+#cloud-config
+chpasswd:
+  list: |
+    root:${random_password.this.result}
+  expire: false
+runcmd:
+    - |
+      curl -sSL -H "X-Config-Type: Cloud" \
+      -H "Authorization: Bearer ${var.bearer}" "https://magenx.sh" | env \
+      PRIVATE_IP=$(curl -s http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address) \
+      SERVER_NAME="${each.key}" \
+      DEBIAN_FRONTEND=noninteractive \
+      TERMS="y" \
+      ENV="${var.env}" \
+      DOMAIN="${var.domain}" \
+      DOWNLOAD_MAGENTO="${var.download_magento}" \
+      VERSION_INSTALLED="${var.version_installed}" \
+      APPLY_MAGENTO_CONFIG="${var.apply_magento_config}" \
+      PHP_VERSION="${var.php_version}" \
+      TIMEZONE="${var.timezone}" \
+      LOCALE="${var.locale}" \
+      CURRENCY="${var.currency}" \
+      ADMIN_FIRST_NAME="${var.admin_first_name}" \
+      ADMIN_LAST_NAME="${var.admin_last_name}" \
+      ADMIN_LOGIN="${var.admin_login}" \
+      ADMIN_EMAIL="${var.admin_email}" \
+%{ if each.key != "frontend" ~}
+      INSTALL_$${SERVER_NAME^^}="y" \
+      $${SERVER_NAME^^}_SERVER_IP="$${PRIVATE_IP}" \
+      bash -s -- lemp ${each.key} firewall
+%{ else ~}
+      INSTALL_NGINX="y" \
+      INSTALL_PHP="y" \
+      MARIADB_SERVER_IP="${digitalocean_droplet.this["mariadb"].ipv4_address_private}" \
+      REDIS_SERVER_IP="${digitalocean_droplet.this["redis"].ipv4_address_private}" \
+      RABBITMQ_SERVER_IP="${digitalocean_droplet.this["rabbitmq"].ipv4_address_private}" \
+      VARNISH_SERVER_IP="${digitalocean_droplet.this["varnish"].ipv4_address_private}" \
+      ELASTICSEARCH_SERVER_IP="${digitalocean_droplet.this["elasticsearch"].ipv4_address_private}" \
+      MEDIA_SERVER_IP="${digitalocean_droplet.this["media"].ipv4_address_private}" \
+      bash -s -- lemp magento install config firewall
+%{ endif ~}
+EOF
+
+    destination = "/tmp/cloud-init-config-${each.key}.yaml"
+  }
 }
 ////////////////////////////////////////////////[ DIGITALOCEAN PROJECT RESOURCES ]////////////////////////////////////////
 
@@ -95,6 +206,6 @@ resource "digitalocean_loadbalancer" "this" {
 resource "digitalocean_project_resources" "this" {
   project   = digitalocean_project.this.id
   resources = [
-    digitalocean_loadbalancer.this.urn
+    concat(values(digitalocean_droplet.services)[*].urn,[digitalocean_volume.this.urn],[digitalocean_loadbalancer.this.urn])
   ]
 }
